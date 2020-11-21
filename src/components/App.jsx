7b0323882e5bd9ec4cbdd364b4deedde11203fc0
Oleg Zharov <levineye@gmail.com>
2020-11-21 17:35:49 +0300

    добавить секцию profile

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Profile from './profile/Profile';
+
+const App = function () {
+  return (
+    <div className="App">
+			<div className="page">
+				<Profile />
+			</div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,14 @@
+const Profile = function () {
+	return (
+		<section className="profile">
+			<img src="#" alt="" className="profile__image" />
+			<div className="profile__info">
+				<p className="profile__name">Oleg</p>
+				<p className="profile__mail">Zharov</p>
+				<a className="profile__button" type="button"></a>
+			</div>
+		</section>
+	);
+}
+
+export default Profile;
