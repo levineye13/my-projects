54378264eb05cebcfe8b66137e28c38ea3b76cb7
Oleg Zharov <levineye@gmail.com>
2020-11-24 18:46:30 +0300

    fix: href у ссылки

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -6,7 +6,7 @@ const Project = function () {
 			<iframe className="projects__site" src="https://levineye13.github.io/russian-travel/"></iframe>
 			<div className="projects__container">
 				<p className="projects__description">Настоящая страна не в выпусках новостей, а здесь.</p>
-				<a className="projects__link" src="https://levineye13.github.io/russian-travel/" target="_blanck">Посмотреть</a>
+				<a className="projects__link" href="https://levineye13.github.io/russian-travel/" target="_blank">Посмотреть</a>
 			</div>
 		</li>
 	);
