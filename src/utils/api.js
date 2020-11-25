e546f832837e043f6fcce3f00d47916b891c95d2
Oleg Zharov <levineye@gmail.com>
2020-11-25 22:54:17 +0300

    feat: добавить класс api, получающий информацию из github

---
+++ src/utils/api.js
@@ -0,0 +1,40 @@
+class Api {
+  constructor({ baseUrl }) {
+    this._baseUrl = baseUrl;
+  }
+
+  _checkValitidyRequest(responce) {
+    if (responce.ok) {
+      return responce.json();
+    }
+    return Promise.reject(new Error(`Ошибка: ${responce.status}`));
+  }
+
+  async getGithubUserInfo() {
+    const responce = await fetch(`${this._baseUrl}levineye13`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const json = this._checkValitidyRequest(responce);
+
+    return json;
+  }
+
+  async getGithubProjects() {
+    const responce = await fetch(`${this._baseUrl}levineye13/repos`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const json = this._checkValitidyRequest(responce);
+
+    return json;
+  }
+}
+
+const api = new Api({ baseUrl: 'https://api.github.com/users/' });
+
+export { api };
