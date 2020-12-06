16fa2551965533e3999c84592cdccebf5d38be75
Oleg Zharov <levineye@gmail.com>
2020-12-06 15:21:18 +0300

    feat: добавить дефолтные значения

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { UserContext } from './../../context/UserContext';
 import './Profile.scss';
-import avatar from './../../images/profile-avatar.jpg';
+import avatar from './../../images/image-not-found.jpg';
 import gitImg from './../../images/profile-git.svg';
 
 const Profile = function () {
@@ -14,8 +14,8 @@ const Profile = function () {
 				<img src={avatar_url || avatar} alt="Фото профиля." className="profile__avatar" />
 			</div>
 			<div className="profile__info">
-        <p className="profile__name">{name}</p>
-        <p className="profile__mail">{email}</p>
+        <p className="profile__name">{name || 'name'}</p>
+        <p className="profile__mail">{email || 'email'}</p>
 				<a className="profile__link" href="https://github.com/levineye13" target="_blanck">
 					<img className="profile__link-img" src={gitImg} alt="Значок Гитхаба." />
 				</a>
