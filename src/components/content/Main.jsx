eefd498bccfd3d46c94f9c86d50174f9742fbf89
Oleg Zharov <levineye@gmail.com>
2020-12-10 21:47:35 +0300

    feat: добавить svg для секции skills

--- src/components/content/Main.jsx
+++ src/components/content/Main.jsx
@@ -1,10 +1,12 @@
 import Projects from '../projects/Projects';
+import Skills from '../skills/Skills';
 import './Main.scss';
 
 const Main = function ({projects}) {
 	return (
 		<main className="content section page__content">
-			<Projects projects={projects} />
+      <Projects projects={projects} />
+      <Skills />
 		</main>
 	);
 }
