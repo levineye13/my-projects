0dafc5e0950da22eb467c7737aaf9b05f391e2a3
Oleg Zharov <levineye@gmail.com>
2021-06-15 15:30:33 +0300

    feat: добавить константы

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,17 @@
+export const GITHUB_API_BASE_URL = 'https://api.github.com';
+
 export const repos = [
-  'mesto',
-  'mesto-react',
-  'mesto-backend',
+  'react-mesto-api-full',
+  'movies-explorer-frontend',
+  'movies-explorer-backend',
   'project-company',
+  'mirea-record-book-frontend',
+  'mirea-record-book-backend',
   'russian-travel',
+  'LaslesVPN',
   'TodoList',
 ];
+
+export const HTTP_METHODS = {
+  GET: 'GET',
+};
