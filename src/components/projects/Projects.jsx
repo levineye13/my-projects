f2c1f162fd4c2886ad1670c1289553c12ab245da
Oleg Zharov <levineye@gmail.com>
2021-06-28 21:10:37 +0300

    feat: поправить форматирование

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
-import Project from './../project/Project';
-import SectionTitle from './../section-title/SectionTitle';
-import './Projects.scss';
+import React from "react";
+import Project from "./../project/Project";
+import SectionTitle from "./../section-title/SectionTitle";
+import "./Projects.scss";
 
-const Projects = function ({projects}) {
-
-	return (
-    <section className="projects section section_narrow content__projects">
-      <SectionTitle text={'Проекты'} className={'projects__section-title'} />
-			<ul className="projects__list">
-				{projects && projects.map(project => <Project props={project} key={project.id} />)}
-			</ul>
-		</section>
-	);
-}
+const Projects = ({ projects }) => {
+  return (
+    <section
+      className="projects section section_narrow content__projects"
+      id="projects"
+    >
+      <SectionTitle text={"Проекты"} className={"projects__section-title"} />
+      <ul className="projects__list">
+        {projects &&
+          projects.map((project) => (
+            <Project props={project} key={project.id} />
+          ))}
+      </ul>
+    </section>
+  );
+};
 
 export default Projects;
--- src/components/section-title/SectionTitle.jsx
+++ src/components/section-title/SectionTitle.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import './SectionTitle.scss';
+import React from "react";
+import "./SectionTitle.scss";
 
-const SectionTitle = function ({text, className}) {
-  return (
-		<h2 className={`section-title ${className}`}>{text}</h2>
-  )
-}
+const SectionTitle = ({ text = "", className = "" }) => {
+  return <h2 className={`section-title ${className}`}>{text}</h2>;
+};
 
 export default SectionTitle;
