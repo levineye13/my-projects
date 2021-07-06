3c9be33e179399f5771066423e2ef472a6263fd5
Oleg Zharov <levineye@gmail.com>
2021-07-06 18:18:43 +0300

    feat: перенести асинхронность в экшены из app

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,8 +1,31 @@
-import { SET_USER_DATA } from './actionTypes';
+import { SET_USER, SET_PROJECTS } from './actionTypes';
 
-export const setUserData = (userData) => {
-  return {
-    type: SET_USER_DATA,
-    payload: userData,
+export const setUser =
+  ({ api, token }) =>
+  async (dispatch) => {
+    const user = await api.getUser(token);
+
+    dispatch({
+      type: SET_USER,
+      payload: {
+        name: user.name,
+        email: user.email,
+        avatarUrl: user.avatar_url,
+      },
+    });
   };
-};
+
+export const setProjects =
+  ({ api, token, reposToDisplay }) =>
+  async (dispatch) => {
+    const repos = await api.getRepositories(token);
+
+    const displayedProjects = repos.filter((item) =>
+      reposToDisplay.includes(item.name)
+    );
+
+    dispatch({
+      type: SET_PROJECTS,
+      payload: [...displayedProjects],
+    });
+  };
