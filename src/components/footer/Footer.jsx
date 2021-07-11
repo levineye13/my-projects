7a794b5e02f10285a79afa24bdd4256cdd18559f
Oleg Zharov <levineye@gmail.com>
2021-07-11 19:02:03 +0300

    feat: в футере получать имя из хранилища, а не копипастить

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,17 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
 import './Footer.scss';
 
 const Footer = ({ mix = '' }) => {
+  const user = useSelector((state) => state.user);
+
   const currentYear = new Date().getFullYear();
 
   return (
     <footer className={`footer ${mix}`}>
-      <p className="footer__copyright">&copy; Олег Жаров. {currentYear}.</p>
+      <p className="footer__copyright">
+        &copy; {user.name}. {currentYear}.
+      </p>
     </footer>
   );
 };
