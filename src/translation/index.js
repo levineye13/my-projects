2eb386cfba0c538257e60a581f1ac39d5a3aa107
Oleg Zharov <levineye@gmail.com>
2021-07-11 18:54:23 +0300

    fix: поправить файлы с переводом текста сайта

---
+++ src/translation/index.js
@@ -0,0 +1,13 @@
+import menu from './menu.json';
+import about from './about.json';
+import projects from './projects.json';
+import skills from './skills.json';
+
+const common = {
+  menu,
+  about,
+  projects,
+  skills,
+};
+
+export default common;
